5f6c28f2828a16945393e1e2cb032a65f56873c9
Raj Shah <shahraj@stanford.edu>
2016-11-26 17:51:31 -0800

    Sensor placement and changes to autopreprocess

---
+++ Sensor_placement_opt/sensorPlacementOpt.m
@@ -0,0 +1,32 @@
+function [ opt_ind ] = sensorPlacementOpt(K, num_img)
+%SENSORPLACEMENTOPT This function finds the top num_img images in the
+%dataset which maximally represents the dataset. It basically implements
+%the sensor placement optimization algorithm
+opt_ind = zeros(num_img, 1);
+SU = zeros(size(K,1),1);
+for n = 1:num_img
+    maxf = realmin;
+    for i = 1:size(K,1)
+        if (SU(i)==0)
+            ind_i = zeros(size(K,1),1); ind_i(i) = 1;
+            ksi = K(logical(SU), logical(ind_i));
+            kui = K(~(logical(SU) | logical(ind_i)), logical(ind_i));
+            Kss = K(logical(SU), logical(SU));
+            Kuu = K(~(logical(SU) | logical(ind_i)), ...
+                ~(logical(SU) | logical(ind_i)));
+            if ~isempty(ksi)
+                f = (1-ksi'/Kss*ksi)/(1-kui'/Kuu*kui);
+            else
+                f = 1/(1-kui'/Kuu*kui);
+            end
+            if f > maxf
+                maxf = f;
+                maxi = i;
+            end
+        end
+    end
+    SU(maxi) = 1;
+    opt_ind(n) = maxi;
+end
+end
+
--- autopreprocess.m
+++ autopreprocess.m
@@ -3,13 +3,14 @@
 % assumption on the 5% of the brightest pixel on each color channel. Then,
 % the contrast of the image is automatically stretched using 0.4% as the
 % lower limit and 99% as the higher limit
-function out_img = autopreprocess(in_img)
-out_img = autowhitebal(in_img);
-out_img = autocontrastadj(out_img);
+function [out_img, autoparams] = autopreprocess(in_img)
+[out_img, Kvals] = autowhitebal(in_img);
+[out_img, con_lims] = autocontrastadj(out_img);
+autoparams = [Kvals; con_lims];
 end
 
 % This function implements auto white balance
-function out_img = autowhitebal(in_img)
+function [out_img, Kvals] = autowhitebal(in_img)
 out_img = zeros(size(in_img));
 Kvals = zeros(3,1);
 for i = 1:3
@@ -24,6 +25,10 @@ out_img(:,:,3) = (Kvals(2)/Kvals(3)).*in_img(:,:,3);
 end
 
 % This function implements auto contrast stretch
-function out_img = autocontrastadj(in_img)
-out_img = imadjust(in_img,stretchlim(in_img, [0.004 0.99]),[]);
+function [out_img, con_lims] = autocontrastadj(in_img)
+out_img = zeros(size(in_img));
+con_lims = stretchlim(rgb2gray(in_img),[0.004 0.99]);
+out_img(:,:,1) = imadjust(in_img(:,:,1),con_lims,[]);
+out_img(:,:,2) = imadjust(in_img(:,:,2),con_lims,[]);
+out_img(:,:,3) = imadjust(in_img(:,:,3),con_lims,[]);
 end
